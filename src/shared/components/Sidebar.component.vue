9a84d7de26c0a587ec967400016f093009e5d0dc
Gabo <u20201e889@upc.edu.pe>
2024-09-27 00:55:20 -0500

    feat: add Sidebar component.

---
+++ src/shared/components/Sidebar.component.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="sidebar">
+    <ul>
+      <li><router-link to="/search-caregivers">Search Caregivers</router-link></li>
+      <li><router-link to="/reservations">Reservations</router-link></li>
+      <li><router-link to="/chat">Chat Online</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<style scoped>
+/* Estilos para el sidebar */
+</style>
