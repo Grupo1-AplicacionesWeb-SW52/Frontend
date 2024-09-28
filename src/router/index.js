9200251cc5a68144d087874014419adf5197d053
Lynn Meza <u20201c320@upc.edu.pe>
2024-09-28 07:53:51 -0500

    update

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const router = createRouter({
 			component: ReservationsPageComponent,
 			meta: { title: 'Reservation' },
 		},
-		{ path: '/', redirect: '/home' },
+		{ path: '/', redirect: '/sign-in-father' },
 	],
 });
 
