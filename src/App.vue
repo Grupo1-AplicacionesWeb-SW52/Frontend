4579cca4cd4f4d0b7bd622d73811f86127b33514
Lynn Meza <u20201c320@upc.edu.pe>
2024-09-28 04:51:53 -0500

    feat: update merge

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
-<template>
-  <div id="app">
-    <SidebarComponent />
-    <router-view />
-  </div>
-</template>
-
 <script>
-import SidebarComponent from '@/shared/components/sidebar.component.vue';
-
 export default {
-  components: {
-    SidebarComponent,
+  name: 'app',
+  title: 'CareNest',
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { label: 'Home', to: '/home' },
+        { label: 'About', to: '/about' },
+        { label: 'Search', to: '/search'},
+        { label: 'Chat Online', to:'/chat'}
+      ]
+    };
   },
-};
-</script>
-
-<style>
-#app {
-  display: flex;
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    }
+  },
+  created() {
+    this.isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+  }
 }
-</style>
+</script>
+<template>
+  <pv-toast></pv-toast>
+  <header v-if="isAuthenticated">
+    <pv-toolbar class="bg-primary" fixed>
+      <template #start>
+        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer" />
+        <h3>CareNest</h3>
+        <div class="flex-column">
+          <router-link v-for="item in items" :key="item.label" v-slot="{ navigate, href}" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
+          </router-link>
+        </div>
+      </template>
+    </pv-toolbar>
+  </header>
+  <pv-sidebar v-model:visible="drawer"/>
+  <router-view/>
+</template>
