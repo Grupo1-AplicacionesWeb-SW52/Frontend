9292a90ab1fe01d8841b2106849930ce6095f8e6
Gabo <u20201e889@upc.edu.pe>
2024-10-24 22:54:05 -0500

    feat: add image to Reservation Card Component.

--- src/reservations/components/ReservationCard.component.vue
+++ src/reservations/components/ReservationCard.component.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="reservation-card">
-    <h3>{{ reservation.caregiver }}</h3>
-    <p><strong>Reservation Date:</strong> {{ reservation.reservationDate }}</p>
-    <p><strong>Total Time:</strong> {{ reservation.totalTimeWork }}</p>
-    <p><strong>Status:</strong> {{ reservation.status }}</p>
+    <div class="image-container">
+      <img :src="reservation.profileImg" alt="Caregiver Image" class="caregiver-image" />
+    </div>
+    <div class="details">
+      <div :class="[statusClass]" class="reservation-status">
+        <p>{{ reservation.status }}</p>
+      </div>
+      <p><strong>Caregiver:</strong> {{ reservation.caregiverName }}</p>
+      <p><strong>Work Location:</strong> {{ reservation.workLocation }}</p>
+      <p><strong>Reservation Date:</strong> {{ reservation.reservationDate }}</p>
+      <p><strong>Total Time Work:</strong> {{ reservation.totalTimeWork }}</p>
+      <p><strong>Total Fare:</strong> {{ reservation.totalFare }}</p>
+    </div>
   </div>
 </template>
 
@@ -13,7 +22,16 @@ export default {
   props: {
     reservation: {
       type: Object,
-      required: true // Esto significa que la prop es obligatoria
+      required: true
+    }
+  },
+  computed: {
+    statusClass() {
+      return {
+        'pending-status': this.reservation.status === 'Pending',
+        'realized-status': this.reservation.status === 'Realized',
+        'cancelled-status': this.reservation.status === 'Cancelled'
+      };
     }
   }
 };
@@ -21,11 +39,46 @@ export default {
 
 <style scoped>
 .reservation-card {
+  display: flex;
   background-color: white;
   border: 1px solid #ccc;
   padding: 16px;
   margin-bottom: 10px;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  position: relative; /* Para posicionar el status */
+}
+
+.image-container {
+  margin-right: 16px; /* Espacio entre la imagen y los detalles */
+}
+
+.caregiver-image {
+  width: 80px; /* Ajusta el tamaño según lo necesites */
+  height: auto;
+  border-radius: 4px; /* Bordes redondeados para la imagen */
+}
+
+.reservation-status {
+  position: absolute;
+  top: 10px;
+  right: 20px; /* Ajusta el espacio a la derecha */
+}
+
+.pending-status {
+  color: black;
+  font-weight: bold;
+}
+.realized-status {
+  color: limegreen;
+  font-weight: bold;
+}
+.cancelled-status {
+  color: red;
+  font-weight: bold;
+}
+
+.details {
+  flex-grow: 1; /* Hace que el contenedor de detalles ocupe el espacio restante */
 }
 </style>
