34cce342b20be625d86dd4d6f46e6f577e97ee21
Arnold24x24 <u201822516@upc.edu.pe>
2024-11-01 01:44:03 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
 export default {
   name: 'app',
-  title: 'SafeChild',
+  title: 'CareNest',
   data() {
     return {
       drawer: false,
       items: [
-        { label: 'Profile Details', to: '/profile/details' },
-        { label: 'Profile Biography', to: '/profile/biography' },
-        { label: 'Profile Header', to: '/profile/header' },
-        { label: 'Profile Service Info', to: '/profile/service-info' }
+        { label: 'Home', to: '/home' },
+        { label: 'About', to: '/about' },
+        { label: 'Search', to: '/search'},
+        {label:'Payment',to:'/payment'},
+        { label: 'Search Caregivers', to: '/search-caregivers' },
+        { label: 'Your Service', to: '/service-detail' },
+        { label: 'Reservations', to: '/reservations' },
+        { label: 'Chat Online', to: '/chat' }
       ]
     };
   },
@@ -17,25 +21,52 @@ export default {
     toggleDrawer() {
       this.drawer = !this.drawer;
     }
+  },
+  created() {
+    this.isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
   }
 }
 </script>
-
 <template>
-  <pv-toast></pv-toast>
-  <header>
-    <pv-toolbar class="bg-primary" fixed>
-      <template #start>
-        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer" />
-        <h3>SafeChild</h3>
-        <div class="flex-column">
-          <router-link v-for="item in items" :key="item.label" v-slot="{ navigate, href }" :to="item.to" custom>
-            <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
-          </router-link>
-        </div>
-      </template>
-    </pv-toolbar>
-  </header>
-  <pv-sidebar v-model:visible="drawer"/>
-  <router-view/>
+  <div id="app">
+    <pv-toast />
+
+    <!-- Header -->
+    <header>
+      <pv-toolbar class="bg-primary" fixed>
+        <template #start>
+          <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer" />
+          <h3 class="title">Care Nest</h3>
+        </template>
+      </pv-toolbar>
+    </header>
+
+    <!-- Sidebar (Drawer) -->
+    <pv-sidebar v-model:visible="drawer" class="p-sidebar-sm">
+      <div class="sidebar-content">
+        <ul>
+          <li v-for="item in items" :key="item.label">
+            <router-link :to="item.to">{{ item.label }}</router-link>
+          </li>
+        </ul>
+      </div>
+    </pv-sidebar>
+
+    <!-- Main Content -->
+    <div class="main-content">
+      <router-view />
+    </div>
+  </div>
 </template>
+<style scoped>
+.bg-primary {
+  background-color: #013b58;
+  color: white;
+}
+
+.sign-in-container {
+  background-color: #013b58;
+  color: white;
+}
+
+</style>
