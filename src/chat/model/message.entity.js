40ef31af886386b8e87ab9e8c23c34ae83a21178
Alejandro Barturen <alebartureng@gmail.com>
2024-11-04 19:06:42 -0500

    Delete src directory

--- src/chat/model/message.entity.js
+++
@@ -1,8 +0,0 @@
-export class Message {
-    constructor({ id, user, lastMessage, lastMessageDate }) {
-        this.id = id;
-        this.user = user;
-        this.lastMessage = lastMessage;
-        this.lastMessageDate = lastMessageDate;
-    }
-}
