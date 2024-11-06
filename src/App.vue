305c4cfb5e8786dbd80f4ec95885bea795a76862
Gabo <u20201e889@upc.edu.pe>
2024-11-05 23:07:45 -0500

    refactor: delete duplicated sidebar.

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,10 @@
     <header>
       <pv-toolbar class="bg-primary" fixed>
         <template #start>
-          <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer" />
           <h3 class="title">Care Nest</h3>
         </template>
       </pv-toolbar>
     </header>
-
-    <!-- Sidebar (Drawer) -->
-    <pv-sidebar v-model:visible="drawer" header="Care Nest">
-      <div class="sidebar-content">
-        <ul class="list-none p-3 m-0">
-          <li v-for="item in items" :key="item.label" class="flex align-items-center p-2 cursor-pointer text-white hover:bg-primary-light">
-            <i :class="item.icon" class="mr-2"></i>
-            <router-link :to="item.to" class="text-white">{{ item.label }}</router-link>
-          </li>
-        </ul>
-      </div>
-    </pv-sidebar>
 
     <!-- Main Content -->
     <div class="main-content">
