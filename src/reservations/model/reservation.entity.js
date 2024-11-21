07d2d599eaf8bbecc7c7e452d5cf04673e304dbe
Gabo <u20201e889@upc.edu.pe>
2024-11-20 23:35:51 -0500

    refactor: correct code lines.

--- src/reservations/model/reservation.entity.js
+++ src/reservations/model/reservation.entity.js
@@ -2,7 +2,7 @@ export class Reservation {
     constructor(id, caregiverId, caregiver, reservationDate, totalTime, status) {
         this.id = id;
         this.caregiverId = caregiverId;
-        this.caregiver = caregiver;
+        this.caregiver = caregiver; // Ahora guardará un objeto con el nombre y la imagen
         this.reservationDate = reservationDate;
         this.totalTime = totalTime;
         this.status = status;
@@ -12,7 +12,7 @@ export class Reservation {
         return new Reservation(
             displayableReservation.id,
             displayableReservation.caregiverId,
-            displayableReservation.caregiver,
+            displayableReservation.caregiver, // Aquí llega el objeto con el nombre y la imagen
             displayableReservation.reservationDate,
             displayableReservation.totalTime,
             displayableReservation.status
@@ -23,7 +23,7 @@ export class Reservation {
         return {
             id: reservation.id,
             caregiverId: reservation.caregiverId,
-            caregiver: reservation.caregiver,
+            caregiver: reservation.caregiver,  // Aquí se devuelve el objeto con los datos del cuidador
             reservationDate: reservation.reservationDate,
             totalTime: reservation.totalTime,
             status: reservation.status
