7ce85aba74f6b83d2e83a4bd9349500686efe499
Gabo <u20201e889@upc.edu.pe>
2024-11-20 23:37:25 -0500

    feat: Add methods to reservations list component.

--- src/reservations/components/ReservationsList.component.vue
+++ src/reservations/components/ReservationsList.component.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <div v-for="reservation in reservations" :key="reservation.id">
-      <ReservationCard :reservation="reservation" />
+      <ReservationCard
+          :reservation="reservation"
+          @cancel-reservation="handleCancelReservation"
+      />
     </div>
   </div>
 </template>
@@ -18,6 +21,14 @@ export default {
     reservations: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    handleCancelReservation(reservationId) {
+      // Aquí puedes implementar la lógica para cancelar la reserva.
+      console.log('Canceling reservation with ID:', reservationId);
+      // Llama al servicio de API para cancelar la reserva
+      this.$emit('cancel-reservation', reservationId);
     }
   }
 };
