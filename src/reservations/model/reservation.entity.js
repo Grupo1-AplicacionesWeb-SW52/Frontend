c4ac93a3c592209b879799f5ef0d1b82b3b13aa5
Gabo <u20201e889@upc.edu.pe>
2024-11-20 22:26:49 -0500

    refactor: delete unused code lines.

--- src/reservations/model/reservation.entity.js
+++ src/reservations/model/reservation.entity.js
@@ -1,18 +1,18 @@
 export class Reservation {
     constructor(id, caregiverId, caregiver, reservationDate, totalTime, status) {
         this.id = id;
-        this.caregiverId = caregiverId;  // ID del cuidador asociado
-        this.caregiver = caregiver; // Nombre del cuidador (ajusta el nombre aquí)
-        this.reservationDate = reservationDate;  // Fecha de la reserva
-        this.totalTime = totalTime;  // Tiempo total de trabajo
-        this.status = status;  // Estado de la reserva
+        this.caregiverId = caregiverId;
+        this.caregiver = caregiver;
+        this.reservationDate = reservationDate;
+        this.totalTime = totalTime;
+        this.status = status;
     }
 
     static fromDisplayableReservation(displayableReservation) {
         return new Reservation(
             displayableReservation.id,
             displayableReservation.caregiverId,
-            displayableReservation.caregiver, // Cambia esto a 'caregiver'
+            displayableReservation.caregiver,
             displayableReservation.reservationDate,
             displayableReservation.totalTime,
             displayableReservation.status
@@ -23,7 +23,7 @@ export class Reservation {
         return {
             id: reservation.id,
             caregiverId: reservation.caregiverId,
-            caregiver: reservation.caregiver,  // Cambia esto a 'caregiver'
+            caregiver: reservation.caregiver,
             reservationDate: reservation.reservationDate,
             totalTime: reservation.totalTime,
             status: reservation.status
